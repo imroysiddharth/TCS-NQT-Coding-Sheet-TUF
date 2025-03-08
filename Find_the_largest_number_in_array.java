50edc29c97b106613ffa363bdb43bb319b63498d
Siddharth Kumar <roysiddharth458@gmail.com>
2025-03-08 19:25:45 +0530

    2nd Code

---
+++ Find_the_largest_number_in_array.java
@@ -0,0 +1,18 @@
+public class Find_the_largest_number_in_array {
+    public static int findLargest(int[] arr){
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0 ; i<arr.length ;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max ;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={10,10,10,4,5,6,7,10,10};
+        System.out.println(findLargest(arr));
+    }
+}
