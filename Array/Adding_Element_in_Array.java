3d7144e2d3801ab08c9de8d35c8fc67e4456ab98
Siddharth Kumar <roysiddharth458@gmail.com>
2025-03-09 12:26:42 +0530

    Adding Element in an Array

---
+++ Array/Adding_Element_in_Array.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Adding_Element_in_Array {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,10};
+        System.out.println(sumElements(arr));
+    }
+
+    public static int sumElements(int arr[]){
+        int sum= 0 ;
+
+        for(int i=0 ; i<arr.length ; i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+}
