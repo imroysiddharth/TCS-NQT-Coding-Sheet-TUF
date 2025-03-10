aa4a9de9a6bc81666024cb7c78e03a8532b23a5f
Siddharth Kumar <roysiddharth458@gmail.com>
2025-03-10 16:04:55 +0530

    taking array input in tcs

---
+++ TcsArrayInput.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TcsArrayInput{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        //String str = "{1,1,1,2,3,4,4,5,6}";
+
+        ArrayList<Integer> al = new ArrayList<>() ;
+        for(int i= 0 ; i<str.length() ; i++){
+            
+            // int n = str.charAt(i);
+            // if(n>47 && n<58){
+            //     System.out.print(n+" ");
+            //     al.add(n-48);
+            // }
+            char n = str.charAt(i);
+            if(n>47 && n<58){
+                System.out.print(n+" ");
+                al.add((int)(n-'0'));
+            }
+        }
+        System.out.println();
+        System.out.println(al);
+
+        int min = Integer.MAX_VALUE ;
+        for(int i:al){
+            if(i<min)min=i;
+        }
+        System.out.println(min);
+        sc.close();
+    }
+}
