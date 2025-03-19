7f9d827c66136ffdcb600f3b0b4a16fa05ea2e77
Siddharth Kumar <roysiddharth458@gmail.com>
2025-03-19 16:00:17 +0530

    Maximum occurring character in a string

---
+++ Strings/MaximumOccurringChar.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class MaximumOccurringChar {
+    public static char cntMaxOccuringChar(String s){
+        int freq[] = new int[26];
+        for(int i=0; i<s.length() ;i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        int cnt = 0 ;
+        char ch = s.charAt(0);
+        for(int i=0; i<freq.length ;i++){
+           if(cnt<freq[i]){
+            cnt=freq[i];
+            ch=(char)(i+'a');
+           }
+        }
+        return ch;
+    }
+    public static void main(String[] args) {
+        String s = "takeuforward";
+       System.out.println(cntMaxOccuringChar(s));
+    }
+
+}
