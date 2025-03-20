97b33f7ff943712fcdfe4e9409f8fbf36a2b9b0f
Siddharth Kumar <roysiddharth458@gmail.com>
2025-03-20 14:17:39 +0530

    Concatenate one string to another

---
+++ Strings/ConcatenateStrings.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class ConcatenateStrings {
+    
+    public static void main(String[] args)
+    {
+        String str1 = "Hello";
+        String str2 = "World";
+        
+        str1 = str1+ str2;
+        
+        System.out.print(str1);
+    }
+}
